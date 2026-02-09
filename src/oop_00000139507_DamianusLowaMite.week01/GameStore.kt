53509e023e46b04bac9aa4c67cb50dd8d87509a4
damian <nspdamian@gmail.com>
2026-02-09 21:02:29 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000139507_DamianusLowaMite.week01/GameStore.kt
+++ src/oop_00000139507_DamianusLowaMite.week01/GameStore.kt
@@ -1,6 +1,16 @@
+fun printReceipt(title: String, finalPrice: Int) {
+    println("=== SteamKW Receipt ===")
+    println("Game Title  : $title")
+    println("Final Price: Rp $finalPrice")
+}
 fun main() {
     val gameTitle = "Cyberpunk 2077"
     val price = 750000
 
     val finalPrice = calculateDiscount(price)
+
+    printReceipt(
+        title = gameTitle,
+        finalPrice = finalPrice
+    )
 }
