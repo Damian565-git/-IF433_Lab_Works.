e6bb3f29857c00753f3efd793b3bcb96b7770be4
damian <nspdamian@gmail.com>
2026-02-17 20:14:18 +0700

    week02: add warning validation in init block

--- src/week02/student.kt
+++ src/week02/student.kt
@@ -4,4 +4,15 @@ class Student(
     val nim: String,
     val name: String,
     var major: String
-)
+) {
+
+    init {
+        if (nim.length != 8) {
+            println("WARNING: NIM harus 8 digit!")
+        }
+
+        if (name.isBlank()) {
+            println("WARNING: Nama tidak boleh kosong!")
+        }
+    }
+}
