80d84fce0574b36ce6b836f9b4221a317d53f0f4
damian <nspdamian@gmail.com>
2026-03-11 10:44:10 +0700

    week06: create Smartwatch with multiple implementations

--- src/Tugas_week06/Capabilities.kt
+++ src/Tugas_week06/Capabilities.kt
@@ -1,4 +1,20 @@
 package oop_139507_Damian.Tugas_week06
 
-class Capabilities {
+// Interface = Can-Do relationship (kemampuan)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+
+    // Default method
+    fun disconnectBluetooth() {
+        println("Bluetooth disconnected")
+    }
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+
+    // Default method
+    fun batteryStatus(): String {
+        return "Battery status: unknown"
+    }
 }
