8a81401857e4f9ccaf22cffb565c66c7dfee9154
damian <nspdamian@gmail.com>
2026-03-11 10:45:35 +0700

    week06: (trial) trigger the diamond problem compiler error

---
+++ src/Tugas_week06/Smartphone.kt
@@ -0,0 +1,6 @@
+package oop_139507_Damian.Tugas_week06
+
+package oop_<nim>_<nama>.week06
+
+// ERROR: Class Smartphone inherits multiple implementations of turnOn()
+// class Smartphone : Camera, Phone { } // Ini akan menyebabkan compiler error
