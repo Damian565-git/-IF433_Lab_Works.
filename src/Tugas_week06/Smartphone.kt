2de98fd5dd5199f8cb21d157aea8e5bcca088ee1
damian <nspdamian@gmail.com>
2026-03-11 10:46:14 +0700

    week06: fix diamond problem using explicit super delegation

--- src/Tugas_week06/Smartphone.kt
+++ src/Tugas_week06/Smartphone.kt
@@ -1,6 +1,16 @@
 package oop_139507_Damian.Tugas_week06
 
-package oop_<nim>_<nama>.week06
+class Smartphone : Camera, Phone {
 
-// ERROR: Class Smartphone inherits multiple implementations of turnOn()
-// class Smartphone : Camera, Phone { } // Ini akan menyebabkan compiler error
+    // Manual override untuk resolve ambiguity
+    override fun turnOn() {
+        super<Camera>.turnOn()  // Menjalankan logika Camera
+        super<Phone>.turnOn()   // Menjalankan logika Phone
+        println("Sistem operasi Smartphone berhasil booting.")
+    }
+
+    // Method spesifik Smartphone
+    fun installApp(appName: String) {
+        println("Menginstall $appName dari Play Store...")
+    }
+}
