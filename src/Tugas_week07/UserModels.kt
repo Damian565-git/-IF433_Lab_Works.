8e600f651dbe050b3ff9198ef4281373e41777eb
damian <nspdamian@gmail.com>
2026-03-30 20:51:15 +0700

    week07: implement data class for structural equality

--- src/Tugas_week07/UserModels.kt
+++ src/Tugas_week07/UserModels.kt
@@ -2,3 +2,6 @@ package oop_139507_Damian.Tugas_week07
 
 // Regular class - tidak punya fungsi equals(), hashCode(), toString() otomatis
 class RegularUser(val name: String, val age: Int)
+
+// Data class - otomatis memiliki equals(), hashCode(), toString(), copy()
+data class DataUser(val name: String, val age: Int)
