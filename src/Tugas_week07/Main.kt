3999c30e57a94f2688e43e358c006151d3b60dda
damian <nspdamian@gmail.com>
2026-03-30 20:51:59 +0700

    week07: implement data class for structural equality

--- src/Tugas_week07/Main.kt
+++ src/Tugas_week07/Main.kt
@@ -13,4 +13,10 @@ println("\n=== TEST REGULAR CLASS ===")
 val reg1 = RegularUser("Alice", 22)
 val reg2 = RegularUser("Alice", 22)
 println(reg1)  // Akan mencetak alamat memori seperti: RegularUser@1a2b3c4d
-println("Sama? ${reg1 == reg2}")  // False - meskipun isi sama
+println("Sama? ${reg1 == reg2}") // False - meskipun isi sama
+
+println("\n=== TEST DATA CLASS ===")
+val data1 = DataUser("Alice", 22)
+val data2 = DataUser("Alice", 22)
+println(data1)  // Output: DataUser(name=Alice, age=22) - readable format
+println("Sama? ${data1 == data2}")  // True - structural equality
