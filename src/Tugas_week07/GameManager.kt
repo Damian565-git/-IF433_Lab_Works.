f9cee114e0cb62126224ea189d48df9c3db74d50
damian <nspdamian@gmail.com>
2026-03-30 21:03:48 +0700

    week07: (task) create GameItem data class

--- src/Tugas_week07/GameManager.kt
+++ src/Tugas_week07/GameManager.kt
@@ -1,2 +1,24 @@
 package oop_139507_Damian.Tugas_week07
 
+// Singleton Game Manager
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+
+    fun stopGame() {
+        if (isGameRunning) {
+            isGameRunning = false
+            println("Menghentikan Game Engine...")
+        } else {
+            println("Game tidak sedang berjalan.")
+        }
+    }
+}
--- src/Tugas_week07/GameModels.kt
+++ src/Tugas_week07/GameModels.kt
@@ -1,2 +1,16 @@
 package oop_139507_Damian.Tugas_week07
 
+// Enum dengan properti custom
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}
+// Data class untuk item game
+data class GameItem(
+    val name: String,
+    val damage: Int,
+    val rarity: ItemRarity
+)
