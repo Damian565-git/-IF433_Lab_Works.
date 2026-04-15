52f4b66779fa421a96784f9d37c3d5cf07cb01fb
damian <nspdamian@gmail.com>
2026-04-16 00:03:06 +0700

    week08: implement chained safe calls and elvis

---
+++ src/week8/Main.kt
@@ -0,0 +1,19 @@
+package oop_139507_Damian.week8
+
+fun main() {
+    // ========== CHECKPOINT 4 ==========
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+    // Rantai Safe Calls yang elegan
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
+    println("Tujuan pengiriman: $destination")
+
+    // ========== CHECKPOINT 5 ==========
+    println("\n=== TEST LET BLOCK ===")
+    val validOrder = Order(null, 250000)
+    val receipt = validOrder.totalPrice?.let { price ->
+        val tax = price * 0.11
+        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di-set!"
+    println(receipt)
+}
