868d7628d230d6d049d808a33e3bddc5a909744a
damian <nspdamian@gmail.com>
2026-05-03 20:53:36 +0700

    week10: (task) populate coin repository

--- src/Tugas_week09/CryptoDashboard.kt
+++ src/Tugas_week09/CryptoDashboard.kt
@@ -1,2 +1,10 @@
 package oop_139507_Damian.Tugas_week09
 
+fun main() {
+    val coinRepo = WalletRepository<Coin>()
+
+    // Menambahkan minimal 3 koin
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 5.0))
+    coinRepo.add(Coin("USDT", 1000.0))
+}
