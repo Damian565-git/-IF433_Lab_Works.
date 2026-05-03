23f96d6a79c7ddf17ea629a80098f1eeb3d002ef
damian <nspdamian@gmail.com>
2026-05-03 20:43:30 +0700

    week10: implement where clause for comparable constraints

--- src/Tugas_week09/NumericalStorage.kt
+++ src/Tugas_week09/NumericalStorage.kt
@@ -1,2 +1,7 @@
 package oop_139507_Damian.Tugas_week09
 
+class MathBox<T : Number>(val value1: T, val value2: T) {
+    fun sum(): Double {
+        return value1.toDouble() + value2.toDouble()
+    }
+}
