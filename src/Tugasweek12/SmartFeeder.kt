c85359f03186a466cd38933c6acb72462dfd76aa
damian <nspdamian@gmail.com>
2026-05-30 15:45:18 +0700

    week12: (task) setup SmartFeeder main function

--- src/Tugasweek12/SmartFeeder.kt
+++ src/Tugasweek12/SmartFeeder.kt
@@ -18,3 +18,41 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     println("Kibble berhasil dikeluarkan!")
     return availableGram - requestedGram
 }
+fun main() {
+    // Setup main system
+    var currentKibbleStock = 50
+
+    // Jadwal Makan 1: Eksekusi dengan Multiple Catch
+    println("=== JADWAL MAKAN PAGI ===")
+    try {
+        currentKibbleStock = dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+    } catch (e: DispenserJamException) {
+        println("Error: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Error: ${e.message}")
+    } catch (e: Exception) {
+        println("Error umum: ${e.message}")
+    } finally {
+        println("Siklus pengecekan dispenser pagi selesai.")
+    }
+
+    // Jadwal Makan 2: Eksekusi dengan runCatching
+    println("\n=== JADWAL MAKAN SORE ===")
+    runCatching {
+        dispenseKibble(
+            requestedGram = 30,
+            availableGram = 1000,
+            isJammed = false
+        )
+    }.onSuccess { newStock ->
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
+    }.onFailure { error ->
+        println("Peringatan ke Pemilik: ${error.message}")
+        println("(Opsional: Berikan chicken jerky secara manual)")
+    }
+}
