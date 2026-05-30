a57ba7cc8b9c564aed4c0582ef579e39a36eabdf
damian <nspdamian@gmail.com>
2026-05-30 15:01:10 +0700

    week11: implement run function for object computation

--- src/Tugasweek11/ScopeLetRun.kt
+++ src/Tugasweek11/ScopeLetRun.kt
@@ -1,2 +1,20 @@
 package oop_139507_Damian.Tugasweek11
 
+fun main() {
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+
+    val length = name?.let {
+        println("Nama terdeteksi: $it")
+        it.length
+    }
+    println("Panjang nama: ${length}")
+}
+fun main() {
+    println("\n=== TEST RUN FUNCTION ===")
+    val result = "Kotlin".run {
+        println("Memproses kata: $this")
+        length * 2
+    }
+    println("Hasil kalkulasi run: $result")
+}
