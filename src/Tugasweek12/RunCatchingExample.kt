0c4ca87dc1c600c0c72ab6909e80d0e33b98ffd5
damian <nspdamian@gmail.com>
2026-05-30 15:40:12 +0700

    week12: handle Result using getOrElse and recover

--- src/Tugasweek12/RunCatchingExample.kt
+++ src/Tugasweek12/RunCatchingExample.kt
@@ -1,2 +1,19 @@
 package oop_139507_Damian.Tugasweek12
 
+fun main() {
+    val result = runCatching {
+        "100".toInt()
+    }.onSuccess { v ->
+        println("Berhasil dikonversi: $v")
+    }.onFailure { e ->
+        println("Gagal konversi: ${e.message}")
+    }
+
+    // Pattern: getOrElse
+    val safeValue = result.getOrElse { -1 }
+    println("Safe Value (getOrElse): $safeValue")
+
+    // Pattern: recover (bisa mengubah tipe kegagalan jadi sukses)
+    val recovered = result.recover { 0 }.getOrNull()
+    println("Recovered Value: $recovered")
+}
