09be7e48d1ce7855a1971bc8bc1c1160b1cd6b80
damian <nspdamian@gmail.com>
2026-05-30 15:02:25 +0700

    week11: implement apply for initialization

--- src/Tugasweek11/ScopeConfiguration.kt
+++ src/Tugasweek11/ScopeConfiguration.kt
@@ -1,2 +1,10 @@
 package oop_139507_Damian.Tugasweek11
 
+ata class User(var name: String = "", var age: Int = 0)
+
+fun main() {
+    val user = User().apply {
+        name = "John"
+        age = 30
+    }
+}
