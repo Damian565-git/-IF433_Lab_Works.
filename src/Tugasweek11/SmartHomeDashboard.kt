b0e004b196164349d2f33c436a9a5771fc1d920e
damian <nspdamian@gmail.com>
2026-05-30 15:10:08 +0700

    week11: (task) configure Philips WiZ using apply

--- src/Tugasweek11/SmartHomeDashboard.kt
+++ src/Tugasweek11/SmartHomeDashboard.kt
@@ -1,2 +1,10 @@
 package oop_139507_Damian.Tugasweek11
 
+fun main() {
+    val homeDevices = mutableListOf<SmartDevice>()
+}
+val lampu = SmartDevice("Philips WiZ Living Room", "Lighting", true, 12).apply {
+    // setup sudah lengkap di constructor
+}.also {
+    homeDevices.add(it)
+}
