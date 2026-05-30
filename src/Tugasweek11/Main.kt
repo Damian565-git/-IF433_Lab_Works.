04b0608aa5b66fbab70b27889f9fb49fa7533ac3
damian <nspdamian@gmail.com>
2026-05-30 14:59:53 +0700

    week11: test extension functions in main

---
+++ src/Tugasweek11/Main.kt
@@ -0,0 +1,9 @@
+package oop_139507_Damian.Tugasweek11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
--- src/Tugasweek11/StringExtensions.kt
+++ src/Tugasweek11/StringExtensions.kt
@@ -1,7 +1,5 @@
 package oop_139507_Damian.Tugasweek11
 
-package oop_001_johnthor.week11
-
 fun String.addGreeting(): String {
     return "Hello, $this!"
 }
