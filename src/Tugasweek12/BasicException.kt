f88146e74ba2a103dddc947d982125cb28fb4294
damian <nspdamian@gmail.com>
2026-05-30 15:56:18 +0700

    week13: implement appendText operation

--- src/Tugasweek12/BasicException.kt
+++ src/Tugasweek12/BasicException.kt
@@ -1,16 +1,2 @@
 package oop_139507_Damian.Tugasweek12
 
-fun divide(a: Int, b: Int): Int {
-    return a / b
-}
-fun main() {
-    println("=== TEST TRY AS EXPRESSION ===")
-    val inputString = "123A"
-    // try langsung mengembalikan hasil parsing, atau -1 jika gagal
-    val result: Int = try {
-        Integer.parseInt(inputString)
-    } catch (e: NumberFormatException) {
-        -1
-    }
-    println("Hasil parsing: $result")
-}
