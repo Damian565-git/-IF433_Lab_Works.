e8f5977d86f1cce7450536626ee72366e9c51967
damian <nspdamian@gmail.com>
2026-05-30 15:12:51 +0700

    week11: (task) calculate total power usage

--- src/Tugasweek11/SmartHomeDashboard.kt
+++ src/Tugasweek11/SmartHomeDashboard.kt
@@ -29,3 +29,7 @@ searchResult?.let {
 with(homeDevices) {
     println("Jumlah perangkat: ${this.size}")
 }
+val totalPower = homeDevices.run {
+    sumOf { it.powerLoad }
+}
+println("Total daya: $totalPower Watt")
